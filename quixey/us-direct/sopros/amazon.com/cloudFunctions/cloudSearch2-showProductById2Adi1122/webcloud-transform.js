8c8bfca8065aa0ab86f15c71fad5e6d24f29b21a
App Portal <tlv-devnet@quixey.com>
2016-04-17 14:02:58 +0300

    updated transform for func://amazon.com/cloudSearch2-showProductById2Adi1122

---
+++ quixey/us-direct/sopros/amazon.com/cloudFunctions/cloudSearch2-showProductById2Adi1122/webcloud-transform.js
@@ -0,0 +1,103 @@
+var transform = function(webcloudOutput) {
+  var transform = {
+    "func://amazon.com/cloudSearch2-showProductById2Adi1122": function(webcloudOutput) {
+      
+        
+      var funcParamsCallback = function(url) {
+        try {
+          return { "url": url };
+        } catch (e) {
+          print(e);
+          return {};
+        }
+      };
+      
+      var parsers = {          "photograph_qxyMoreInfo": function(str) {
+            try {
+              return 'popo';
+            } catch(e) {}
+            return str;
+          },
+          "expires": function(str) {
+            try {
+              return '2016-04-11T17:31:22Z';
+            } catch(e) {}
+            return str;
+          }
+      };
+
+      var results = [];
+
+      for (var i = 0; i < webcloudOutput.length ; i++) {
+
+        var data = webcloudOutput[i];
+        var cloudFunc = "func://amazon.com/cloudSearch2-showProductById2Adi1122";
+
+        var func = "func://amazon.com/showProductById2";
+
+        var doc = parseDocument(data.content);
+
+        var params = {};
+        for (var key in data) {
+          if (keysToExclude.indexOf(key) < 0) {
+            params[key] = data[key];
+          }
+        }
+
+        var output = {
+          "displayContent": {}
+        };
+
+        
+        addComputedValue(output.displayContent, "@vertical", ["//div[contains(@class, 'sx-table-detail')]/div[contains(@class, 'a-section a-spacing-micro')]/div[contains(@class, 'a-row a-size-small a-color-secondary')]"], parsers, doc);
+        addComputedValue(output.displayContent, "category", ["//div[contains(@class, 'sx-table-detail')]/div[contains(@class, 'a-row a-spacing-none a-size-small a-color-secondary')]"], parsers, doc);
+        addComputedValue(output.displayContent, "description", ["//div[contains(@class, 'a-size-mini')]"], parsers, doc);
+        addComputedValue(output.displayContent, "expires", ["//div[contains(@class, 'sx-table-detail')]/div[contains(@class, 'a-row a-spacing-none a-size-small a-color-secondary')]"], parsers, doc);
+        addComputedValue(output.displayContent, "name", ["//h5[contains(@class, 'a-size-base')]"], parsers, doc);
+        addComputedValue(output.displayContent, "photograph_author", ["//div[contains(@class, 'sx-table-detail')]/div[contains(@class, 'a-row a-spacing-micro a-color-secondary')]/span[contains(@class, 'a-size-small')]", "//div[contains(@class, 'sx-table-detail')]/div[contains(@class, 'a-row a-spacing-micro a-color-secondary')]/span[contains(@class, 'a-size-small')]"], parsers, doc);
+        addComputedValue(output.displayContent, "photograph_description", ["//div[contains(@class, 'a-size-mini')]"], parsers, doc);
+        addComputedValue(output.displayContent, "photograph_image", ["//img[contains(@class, 'sx-product-image')]"], parsers, doc);
+        addComputedValue(output.displayContent, "photograph_name", ["//h5[contains(@class, 'a-size-base')]"], parsers, doc);
+        addComputedValue(output.displayContent, "photograph_qxyInfo", ["//span[contains(@class, 'a-text-bold')]"], parsers, doc);
+        addComputedValue(output.displayContent, "photograph_qxyMoreInfo", ["//div[contains(@class, 's-usedandnew')]/span[contains(@class, 'a-color-price')]"], parsers, doc);
+        addComputedValue(output.displayContent, "price", ["//span[contains(@class, 'a-text-bold')]"], parsers, doc);
+        addComputedValue(output.displayContent, "quality", ["//div[contains(@class, 'sx-table-detail')]/div[contains(@class, 'a-row a-spacing-none a-size-small')]"], parsers, doc);
+
+
+        if (params) {
+          output = assign(output, params);
+        }
+
+        if (typeof(funcParamsCallback) === "function" && typeof(output.webUrl) === "string") {
+          var funcParams = funcParamsCallback(output.webUrl);
+
+          output.displayContent = assign(output.displayContent, {
+            "@id": populateFurl("func://amazon.com/showProductById2/{url}", funcParams)
+          });
+
+          output.displayContent = assign(output.displayContent, funcParams)
+        }
+
+
+        output.displayContent = assign(output.displayContent, {
+          "@type": "Product",
+          "crawled": utcNow(),
+          "created": utcNow(),
+          "httpStatusCode": 200,
+          "url": output.webUrl
+        });
+        output.entitySchema = "product";
+        output.useEntitySchema = true;
+
+        results.push({ "function": func, "deepViewContent": output });
+      }
+
+      return results;
+    }
+  };
+
+  return transform["func://amazon.com/cloudSearch2-showProductById2Adi1122"](webcloudOutput);
+}
+
+
+

e42465d483291065b96ee47744bbaba0e7533d10
App Portal <devportalgithub@quixey.com>
2016-05-03 15:43:46 +0300

    updated transform for func://amazon.com/cloudSearch2-showProductById2

---
+++ quixey/us-direct/sopros/amazon.com/cloudFunctions/cloudSearch2-showProductById2/webcloud-transform.js
@@ -0,0 +1,77 @@
+var transform = function(cloudOutput, ttl) {
+  var transform = {
+    "func://amazon.com/cloudSearch2-showProductById2": function(cloudOutput) {
+      
+          
+      var parsers = {      };
+
+      var results = [];
+
+      for (var i = 0; i < cloudOutput.length ; i++) {
+
+        var data = cloudOutput[i];
+        var cloudFunc = "func://amazon.com/cloudSearch2-showProductById2";
+
+        var func = "func://amazon.com/showProductById2";
+
+        var doc = parseDocument(data.content);
+
+        var params = {};
+        for (var key in data) {
+          if (keysToExclude.indexOf(key) < 0) {
+            params[key] = data[key];
+          }
+        }
+
+        var output = {
+          "displayContent": {}
+        };
+
+        
+  
+        addComputedValue(output.displayContent, "@vertical", ["//div[contains(@class, 'sx-table-detail')]/div[contains(@class, 'a-section a-spacing-micro')]/div[contains(@class, 'a-row a-size-small a-color-secondary')]"], parsers, doc);
+        addComputedValue(output.displayContent, "image", ["//img[contains(@class, 'sx-product-image')]"], parsers, doc);
+        addComputedValue(output.displayContent, "name", ["//h5[contains(@class, 'a-size-base')]"], parsers, doc);
+        addComputedValue(output.displayContent, "price", ["//div[contains(@class, 's-usedandnew')]/span[contains(@class, 'a-color-price')]"], parsers, doc);
+        addComputedValue(output.displayContent, "product_image", ["//img[contains(@class, 'sx-product-image')]"], parsers, doc);
+        addComputedValue(output.displayContent, "product_name", ["//h5[contains(@class, 'a-size-base')]"], parsers, doc);
+
+
+        if (params) {
+          output = assign(output, params);
+        }
+
+        if (typeof(funcParamsCallback) === "function" && typeof(output.webUrl) === "string") {
+          var funcParams = funcParamsCallback(output.webUrl);
+
+          output.displayContent = assign(output.displayContent, {
+            "@id": populateFurl("func://amazon.com/showProductById2/{url}", funcParams)
+          });
+
+          output.displayContent = assign(output.displayContent, funcParams)
+        }
+
+
+        output.displayContent = assign(output.displayContent, {
+          "@type": "Product",
+          "crawled": utcNow(),
+          "created": utcNow(),
+          "httpStatusCode": 200,
+          "expires": utcNow(ttl || 60),
+          "url": output.webUrl
+        });
+        output.entitySchema = "product";
+        output.useEntitySchema = true;
+
+        results.push({ "function": func, "deepViewContent": output });
+      }
+
+      return results;
+    }
+  };
+
+  return transform["func://amazon.com/cloudSearch2-showProductById2"](cloudOutput);
+}
+
+
+
